9ab9789b1fe8a5650703cf47a6d00a0535e4d2ee
Abhishek Kumar <abhishekk580@gmail.com>
2023-11-10 00:07:26 +0530

    added product model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from product.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ["id", "name", "slug"]
+    list_display_links = ["id", "name"]
+
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.CharField(max_length=128)
+    image_url = models.CharField(max_length=160)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-created_on"]
